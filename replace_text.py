d4b7529410a3352a48e167f57287baeb05255b81
Mikael Mayo <mikael@Mikaels-MacBook-Pro.local>
2020-10-26 17:00:53 -0700

    updated replace_text

--- replace_text.py
+++ replace_text.py
@@ -1,6 +1,6 @@
 with open("text.txt", "r") as readText:
     readData = readText.read()
-    readData = readData.replace('xx', 'oo')
+    readData = readData.replace('oo', 'ooo')
 with open("text.txt", "w") as writeText:
     writeText.write(readData)
 
